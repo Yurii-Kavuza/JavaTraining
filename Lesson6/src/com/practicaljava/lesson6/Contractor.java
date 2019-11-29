e0f9b1040875391c0b297dceef4f1d50cc932d62
Yurii-Kavuza <kavuzaiurii@gmail.com>
2019-11-29 16:04:18 +0200

    Visual part of my Calculator

--- Lesson6/src/com/practicaljava/lesson6/Contractor.java
+++ Lesson6/src/com/practicaljava/lesson6/Contractor.java
@@ -4,16 +4,15 @@ public class Contractor extends Person implements Payable {
 
 	public Contractor(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean increasePay(int percent) {
-		if (percent<=INCREASE_CAP) {
+		if (percent<=Payable.INCREASE_CAP) {
 			System.out.println("Increasing salary by " + percent + "%. " + getName());
 			return true;
 		}else {
-			System.out.println("Sorry, can't increase hourly rate by more than 20%. Person's name is " + getName());
+			System.out.println("Sorry, can't increase hourly rate by more than "+ Payable.INCREASE_CAP +"%. " + getName());
 			return false;
 		}
 	}
--- Lesson6/src/com/practicaljava/lesson6/Employee.java
+++ Lesson6/src/com/practicaljava/lesson6/Employee.java
@@ -1,12 +1,9 @@
 package com.practicaljava.lesson6;
 
-public class Employee extends Person implements Payable {
-
-	
+public class Employee extends Person implements Payable {	
 
 	public Employee(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override

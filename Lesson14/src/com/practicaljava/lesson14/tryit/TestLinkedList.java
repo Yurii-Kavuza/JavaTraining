224bff402b2b22391965e94bcb6f446f2ea0a50c
Yurii-Kavuza <kavuzaiurii@gmail.com>
2019-12-23 18:24:39 +0200

    Intro to Collections

---
+++ Lesson14/src/com/practicaljava/lesson14/tryit/TestLinkedList.java
@@ -0,0 +1,25 @@
+package com.practicaljava.lesson14.tryit;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class TestLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList passengerList = new LinkedList();
+		passengerList.add("Alex Smith");
+		passengerList.add("Mary Lou");
+		passengerList.add("Sim Monk");
+		
+		//Get the lisr iterator and print every element of the list
+		ListIterator iterator = passengerList.listIterator();
+		
+		System.out.println(iterator.next());
+		passengerList.add("VIP customer");
+		//iterator.previous();
+		System.out.println(iterator.next());
+		System.out.println(iterator.next());
+		System.out.println(iterator.next());
+	}
+
+}
---
+++ Test/src/Lesson14/TestArrayList.java
@@ -0,0 +1,14 @@
+package Lesson14;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		int i = 1;            //1 
+		i = -+-(10 + 2 + i);   //2
+		
+		//++i--;                //3 
+		System.out.println(i); 
+
+	}
+
+}

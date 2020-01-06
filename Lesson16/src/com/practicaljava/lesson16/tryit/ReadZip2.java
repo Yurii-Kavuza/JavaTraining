8387945ee59acc1465d82dfc91867df4ce9c9748
Yurii-Kavuza <kavuzaiurii@gmail.com>
2020-01-06 18:01:12 +0200

    Correction new variant to work with zip-file

--- Lesson16/src/com/practicaljava/lesson16/tryit/ReadZip2.java
+++ Lesson16/src/com/practicaljava/lesson16/tryit/ReadZip2.java
@@ -3,15 +3,11 @@ package com.practicaljava.lesson16.tryit;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ReadZip2 {
 
 	public static void main(String[] args) throws IOException {
 		Path zipfile = Paths.get("Desktop.zip");
-		Map<String, String> env = new HashMap<>();
-		env.put("create", "true");
 		FileSystem fs = FileSystems.newFileSystem(zipfile, (ClassLoader) null);
 		Files.walkFileTree(fs.getPath("/"), new SimpleFileVisitor<Path>()
 		{
